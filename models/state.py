3ea10a5a71a585c6c857af9bb93359ee61634c58
Julia Bullard <5534@holbertonstudents.com>
2023-03-03 17:18:18 -0600

    Updated if/else statements in State

--- models/state.py
+++ models/state.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 from sqlalchemy.orm import relationship
-from models.city import City
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from os import getenv
 
-class State(BaseModel):
-    """ State class """
+
+class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan", backref="state")
-    
-    @property
-    def cities(self):
-        """Returns the list of City instances with state_id equal to the current State.id"""
-        from models import storage
-        from models.city import City
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            city_list = []
+            for city in models.storage.all('City').values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
