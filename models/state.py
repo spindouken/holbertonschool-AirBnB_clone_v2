10fa3b816d8054324eef2917a0449ce9140df1e7
Julia Bullard <5534@holbertonstudents.com>
2023-03-03 17:40:38 -0600

    Update state

--- models/state.py
+++ models/state.py
@@ -8,13 +8,3 @@ from os import getenv
 class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        @property
-        def cities(self):
-            city_list = []
-            for city in models.storage.all('City').values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
