2b672383c493865ee9c736fb8c692ed5564d25ec
Julia Bullard <5534@holbertonstudents.com>
2023-03-03 23:53:31 -0600

    File Storage

--- console.py
+++ console.py
@@ -2,7 +2,6 @@
 """ Console Module """
 import cmd
 import sys
-from models import *
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.__init__ import storage
