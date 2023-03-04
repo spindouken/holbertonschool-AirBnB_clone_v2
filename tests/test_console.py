b8630cdfa8cceab2dd49ee74331ae3a65e073dd4
spindouken <mlc9181209@gmail.com>
2023-03-04 17:08:06 -0600

    added proto console test file

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Console tests"""
+import unittest
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """Will contain unittests for HBNB command interpreter"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
