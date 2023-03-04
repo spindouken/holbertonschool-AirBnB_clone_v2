b9e2ec0e0fcf05bf5069a3511bbbc67cb437ef68
spindouken <mlc9181209@gmail.com>
2023-03-04 13:38:24 -0600

    filestorage delete fix attempt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -54,7 +54,6 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Deletes obj from __objects if it's inside"""
-        if obj is not None and \
-           "{}.{}".format(type(obj).__name__, obj.id) in self.__objects:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-            self.save()
+        if (obj):
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
