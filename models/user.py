a3a21ebb3d820c4b2dc67693359d330ee5e68643
Julia Bullard <5534@holbertonstudents.com>
2023-03-05 14:09:36 -0600

    Updated Task 8 files

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
@@ -12,3 +14,17 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship("Place", backref="user", cascade="all, delete")
+    else:
+        @property
+        def places(self):
+            """ Getter attribute in case of file storage """
+            from models import storage
+            from models.place import Place
+            place_list = []
+            for place in storage.all(Place).values():
+                if place.user_id == self.id:
+                    place_list.append(place)
+            return place_list
