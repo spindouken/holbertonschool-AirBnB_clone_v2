1fb9e21f3279c548f01a650c5baa0da40d097c63
spindouken <mlc9181209@gmail.com>
2023-03-06 13:40:33 -0600

    init and basemodel changes for db test check hunt

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """Module that creates a unique FileStorage instance for the application"""
-
-import os
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from os import getenv
 
 
-if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+if getenv("HBNB_TYPE_STORAGE") == 'db':
     storage = DBStorage()
-    storage.reload()
 else:
     storage = FileStorage()
-    storage.reload()
+storage.reload()
