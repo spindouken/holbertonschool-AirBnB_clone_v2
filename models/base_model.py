c2471078f563fc19147adc3284087c7eb364e9d2
spindouken <mlc9181209@gmail.com>
2023-03-06 13:53:21 -0600

    revert base model changes

--- models/base_model.py
+++ models/base_model.py
@@ -16,14 +16,24 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        if kwargs:
-            for ky, vl in kwargs.items():
-                if ky in ('created_at', 'updated_at'):
-                    vl = datetime.strptime(vl, '%Y-%m-%dT%H:%M:%S.%f')
-                if ky != '__class__':
-                    setattr(self, ky, vl)
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
         """Returns a string representation of the instance"""
