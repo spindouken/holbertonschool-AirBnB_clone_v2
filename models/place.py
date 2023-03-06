7970af44bd7c64040a86f2dd2014a6a1af059840
spindouken <mlc9181209@gmail.com>
2023-03-06 01:44:11 -0600

    amenity and place attempt over 9000

--- models/place.py
+++ models/place.py
@@ -8,7 +8,6 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
-
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
                              ForeignKey('places.id'),
@@ -21,23 +20,22 @@ place_amenity = Table('place_amenity', Base.metadata,
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
 
-    reviews = relationship('Review',
-                               cascade="all, delete, delete-orphan",
-                               backref="place")
+    reviews = relationship('Review', backref='place', cascade='delete')
     amenities = relationship("Amenity",
-                                 secondary=place_amenity,
+                                 secondary='place_amenity',
                                  back_populates='place_amenities',
                                  viewonly=False)
 
@@ -65,7 +63,5 @@ class Place(BaseModel, Base):
         @amenities.setter
         def amenities(self, amenity_list):
             """setter"""
-            if all(isinstance(amenity, Amenity) for amenity in amenity_list):
-                self.amenity_ids = [amenity.id for amenity in amenity_list]
-            else:
-                return
+            if type(obj) == Amenity:
+                self.amenities_ids.append(obj.id)
