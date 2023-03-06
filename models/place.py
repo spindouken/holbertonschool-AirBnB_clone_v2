4fc1f44317bd3b3e3a6e408ae925b0c0839da53f
spindouken <mlc9181209@gmail.com>
2023-03-06 01:09:44 -0600

    place revert attempt

--- models/place.py
+++ models/place.py
@@ -5,10 +5,8 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
-
 place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
+                      Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
@@ -17,24 +15,10 @@ place_amenity = Table('place_amenity', Base.metadata,
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
-
-    storageType = getenv("HBNB_TYPE_STORAGE")
-    if storageType == "db":
-        reviews = relationship('Review',
-                               cascade="all, delete, delete-orphan",
-                               backref="place")
+    @@ -24,6 +31,11 @@ class Place(BaseModel, Base):
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", backref="place",
+                               cascade="all, delete")
         amenities = relationship("Amenity",
                                  secondary=place_amenity,
                                  back_populates='place_amenities',
@@ -43,32 +27,11 @@ class Place(BaseModel, Base):
     else:
         @property
         def reviews(self):
-            """ Review Getter attribute in case of file storage """
+            """ Getter attribute in case of file storage """
+            from models import storage
             from models.review import Review
             review_list = []
             for review in storage.all(Review).values():
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
-
-        @property
-        def amenities(self):
-            """getter"""
-            from models.amenity import Amenity
-            ame = []
-            moby = storage.all(Amenity)
-
-            for amenity_inst in moby.values():
-                if amenity_inst.id == self.amenity_id:
-                    ame.append(amenity_inst)
-            return ame
-
-        @amenities.setter
-        def amenities(self, amenity_list):
-            """setter"""
-            from models.amenity import Amenity
-            for x in amenity_list:
-                if type(x) == Amenity:
-                    self.amenity_ids.append(x)
- 
- from models import storage
