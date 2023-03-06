3d262c7a84dfdf8bf744ca9a9cac36b54d03b3ed
Julia Bullard <5534@holbertonstudents.com>
2023-03-05 22:37:55 -0600

    Fixed user.py

--- models/user.py
+++ models/user.py
@@ -9,22 +9,11 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship("Place", backref="user", cascade="all, delete")
-    else:
-        @property
-        def places(self):
-            """ Getter attribute in case of file storage """
-            from models import storage
-            from models.place import Place
-            place_list = []
-            for place in storage.all(Place).values():
-                if place.user_id == self.id:
-                    place_list.append(place)
-            return place_list
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user',
+                          cascade='all, delete, delete-orphan')
+    reviews = relationship('Review', backref='user',
+                           cascade='all, delete, delete-orphan')
