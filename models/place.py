0fa2bcc835aead10c540844f477efe53f5ea1a33
Julia Bullard <5534@holbertonstudents.com>
2023-03-05 23:50:48 -0600

    lots of pycode issues in place

--- models/place.py
+++ models/place.py
@@ -7,33 +7,31 @@ from os import getenv
 
 
 class Place(BaseModel, Base):
-   """ A place to stay """
-   __tablename__ = 'places'
-   city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-   user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-   name = Column(String(128), nullable=False)
-   description = Column(String(1024))
-   number_rooms = Column(Integer, default=0, nullable=False)
-   number_bathrooms = Column(Integer, default=0, nullable=False)
-   max_guest = Column(Integer, default=0, nullable=False)
-   price_by_night = Column(Integer, default=0, nullable=False)
-   latitude = Column(Float)
-   longitude = Column(Float)
-   amenity_ids = []
+    """ A place to stay """
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = []
 
-
-   if getenv('HBNB_TYPE_STORAGE') == 'db':
-       reviews = relationship("Review", backref="place",
-                              cascade="all, delete")
-   else:
-       @property
-       def reviews(self):
-           """ Getter attribute in case of file storage """
-           from models import storage
-           from models.review import Review
-           review_list = []
-           for review in storage.all(Review).values():
-               if review.place_id == self.id:
-                   review_list.append(review)
-           return review_list
-
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", backref="place",
+                               cascade="all, delete")
+    else:
+        @property
+        def reviews(self):
+            """ Getter attribute in case of file storage """
+            from models import storage
+            from models.review import Review
+            review_list = []
+            for review in storage.all(Review).values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
