91e26830c80099b53ce5567b8adb67958aa1bf04
Julia Bullard <5534@holbertonstudents.com>
2023-03-05 22:47:54 -0600

    I think I fixed my mess up

--- models/city.py
+++ models/city.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-"""This is the city class"""
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from os import getenv
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
+    """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(
-            String(128),
-            nullable=False)
-        state_id = Column(
-            String(60),
-            ForeignKey('states.id'),
-            nullable=False)
-        places = relationship(
-            "Place",
-            backref="cities",
-            cascade="all, delete-orphan")
-    else:
-        name = ""
-        state_id = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete, delete-orphan')
