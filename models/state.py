0bd3aa5c65f18f003b852953898e46c719ed5dbc
Julia Bullard <5534@holbertonstudents.com>
2023-03-05 22:42:18 -0600

    fixed state.py

--- models/state.py
+++ models/state.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class """
     __tablename__ = "states"
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete-orphan",
-                              backref="state")
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """getter - returns the list of City instances"""
-            return [obj for (key, obj) in
-                    models.storage.all(models.City).items()
-                    if obj.state_id == self.id]
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
