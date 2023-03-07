4adba53f6a2662b814b2fa8ba6fb5df125fe204a
spindouken <mlc9181209@gmail.com>
2023-03-06 21:25:44 -0600

    compliance

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -13,19 +13,19 @@ class test_review(test_basemodel):
         self.name = "Review"
         self.value = Review
 
-def test_place_id(self):
-    """Test the place_id attribute of the Review class"""
-    new = Review()
-    new.place_id = "test_place_id"
-    self.assertEqual(type(new.place_id), str)
+    def test_place_id(self):
+        """Test the place_id attribute of the Review class"""
+        new = Review()
+        new.place_id = "test_place_id"
+        self.assertEqual(type(new.place_id), str)
 
-def test_user_id(self):
-    """ Test that user_id is a string """
-    new_user = User(email="john@example.com", password="password")
-    new_user.save()
-    new_review = Review(place_id="123", user_id=new_user.id, text="Test review")
-    new_review.save()
-    self.assertEqual(type(new_review.user_id), str)
+    def test_user_id(self):
+        """ Test that user_id is a string """
+        new_user = User(email="john@example.com", password="password")
+        new_user.save()
+        new_review = Review(place_id="123", user_id=new_user.id, text="Test review")
+        new_review.save()
+        self.assertEqual(type(new_review.user_id), str)
 
     def test_text(self):
         """ """
