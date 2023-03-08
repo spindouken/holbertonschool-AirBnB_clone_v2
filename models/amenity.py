b1aadb216e19e352d004a5b58aefee9b1069b46f
Julia Bullard <5534@holbertonstudents.com>
2023-03-08 09:29:10 -0600

    JUST WAIT MASON

--- models/amenity.py
+++ models/amenity.py
@@ -6,8 +6,7 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
+    from models.place import place_amenity
     __tablename__ = 'amenities'
-
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   viewonly=False)
+    place_amenities = relationship('Place', secondary='place_amenity')
