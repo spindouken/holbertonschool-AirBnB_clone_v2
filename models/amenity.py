91f8d3add2741e3c3e036a86ea38eebeb815f5fc
Julia Bullard <5534@holbertonstudents.com>
2023-03-08 09:35:42 -0600

    Revert revert

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    from models.place import place_amenity
     __tablename__ = 'amenities'
+    
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity)
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                   viewonly=False)
