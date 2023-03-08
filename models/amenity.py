1cfb6ed98e8e9ad2575268b6d0d150f06a281c6f
Julia Bullard <5534@holbertonstudents.com>
2023-03-08 10:05:35 -0600

    Reverting to 'place_amenity' from place_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,10 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity,
+    place_amenities = relationship('Place', secondary='place_amenity',
                                    viewonly=False)
