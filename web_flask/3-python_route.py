c59b3aef5eb50078eeb40f33cb5b281f228001cb
spindouken <mlc9181209@gmail.com>
2023-03-24 13:17:30 -0500

    add methods=['GET']

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,26 +5,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/", methods=['GET'], strict_slashes=False)
 def holla_holla():
     """displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb", methods=['GET'], strict_slashes=False)
 def hbnb():
     """displays 'HBNB'"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>", methods=['GET'], strict_slashes=False)
 def c_text_value(text):
     """display “C ” followed by the value of the text variable"""
     return f"C {text.replace('_', ' ')}"
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', methods=['GET'], strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
 def python_text_value(text="is cool"):
     """displays "Python " followed by the text value (of declared variable)"""
     return f"Python {text.replace('_', ' ')}"
