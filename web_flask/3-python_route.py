f92048cb05b242827059692029305c041a131fbe
spindouken <mlc9181209@gmail.com>
2023-03-24 12:49:31 -0500

    slight structure change, code running but wrong?xD

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c_text_value(text):
     return f"C {text.replace('_', ' ')}"
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text_value(text="is cool"):
     """displays "Python " followed by the text value (of declared variable)"""
