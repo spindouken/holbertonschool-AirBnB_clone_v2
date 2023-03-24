adf6475ccfe8be7ccd191f07b1d0d780624bc864
spindouken <mlc9181209@gmail.com>
2023-03-24 14:01:18 -0500

    convert to .format style

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application
-listening on 0.0.0.0:5000"""
+listening on 0.0.0.0:5000
+"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -12,26 +13,31 @@ def holla_holla():
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
+def hbnbizzle():
     """displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_text_value(text):
+def c_walk(text):
     """display “C ” followed by the value of the text variable"""
-    return f"C {text.replace('_', ' ')}"
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text_value(text):
-    return f"Python {text.replace('_', ' ')}"
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_yaya(text="is cool"):
+    """
+    display “Python ” followed by the value of the text variable
+    and replace underscore _ symbols with a space
+    """
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
-    return f"{n} is a number"
+    """display "n is a number" only if n is an interger"""
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
