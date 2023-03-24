2c9acb1c881795cd7dd9198791e6e9a41f3d4bb6
spindouken <mlc9181209@gmail.com>
2023-03-24 13:54:22 -0500

    python is yaya

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 """script that starts a Flask web application
-listening on 0.0.0.0:5000"""
+listening on 0.0.0.0:5000
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def holla_holla():
+    """displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
+def hbnbizzle():
+    """displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_text_value(text):
-    return "C " + text.replace("_", " ")
+def c_walk(text):
+    """display “C ” followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text_value(text="is cool"):
-    return "Python " + text.replace("_", " ")
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_yaya(text="is cool"):
+    """display “Python ” followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
