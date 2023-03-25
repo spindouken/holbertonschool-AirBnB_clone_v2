f50d592ba86a7b821243bacdb64c64a62e314744
spindouken <mlc9181209@gmail.com>
2023-03-25 01:09:00 -0500

    basic setups

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+listening on 0.0.0.0:5000
+"""
+from models import storage
+from flask import render_template, Flask
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """
+    placeholder
+    """
+    states = storage.all('State')
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """
+    placeholder
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
