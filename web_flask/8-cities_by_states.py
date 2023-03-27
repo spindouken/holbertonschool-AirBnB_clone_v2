68916a02c0bf65770be19c8d13d0b9dc3851eb67
spindouken <mlc9181209@gmail.com>
2023-03-27 18:45:11 -0500

    code running locally but no states in list

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application
+"""Script that starts a Flask web application
 listening on 0.0.0.0:5000
 """
+from flask import Flask, render_template
 from models import storage
-from flask import render_template, Flask
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """
-    placeholder
-    """
-    states = storage.all('State')
-    return render_template("8-cities_by_states.html", states=states)
+    """Display a HTML page with a list of all State objects
+    present in DBStorage sorted by name (A->Z) and their cities"""
+    states = storage.all(State)
+    sorted_states_list = sorted(states.values(), key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=sorted_states_list)
 
 
 @app.teardown_appcontext
 def close_storage(exception):
-    """
-    placeholder
-    """
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port="5000")
