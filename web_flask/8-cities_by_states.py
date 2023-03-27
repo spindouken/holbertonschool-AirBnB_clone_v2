b1f4405880c8f3a645efa641cc61bc8f79857617
spindouken <mlc9181209@gmail.com>
2023-03-27 18:50:08 -0500

    pycodestyle compliance

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,7 +14,8 @@ def cities_by_states():
     present in DBStorage sorted by name (A->Z) and their cities"""
     states = storage.all(State)
     sorted_states_list = sorted(states.values(), key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states_list)
+    return render_template('8-cities_by_states.html',
+                           states=sorted_states_list)
 
 
 @app.teardown_appcontext
